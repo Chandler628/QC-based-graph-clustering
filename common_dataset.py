e19a3cf86606e1ea73b7a8c8fab24c7dad1d3145
wangzhe <wangzhe@kongfoo.cn>
2023-08-08 11:02:14 +0800

    梯度下降算法更新

--- common_dataset.py
+++ common_dataset.py
@@ -1,12 +1,11 @@
 import time
-
 import pandas as pd
 import networkx as nx
 import numpy as np
 import torch
 import math
 import matplotlib.pyplot as plt
-# from numba import cuda
+from numba import cuda
 from pandas import DataFrame
 from sklearn import metrics
 from scipy.spatial.distance import squareform
@@ -19,55 +18,48 @@ from utils.data_processor import normalize_adj
 import clustering
 
 
-# @cuda.jit
-# def potential_gpu(y, num, dense_matrix, Delta):
-#     idx = cuda.threadIdx.x + cuda.blockDim.x * cuda.blockIdx.x
-#     if idx < num:
-#         sum1 = 0
-#         sum2 = 0
-#         for i in range(num):
-#             sum1 += (dense_matrix[idx][i] ** 2) * math.exp((-dense_matrix[idx][i] ** 2) / (2 * (Delta ** 2)))
-#             sum2 += math.exp((-dense_matrix[idx][i] ** 2) / (2 * (Delta ** 2)))
-#         y[idx] = (1 / (2 * (Delta ** 2))) * (sum1 / sum2)
 
-def potential(Delta, dense_matrix):
-
-    t = torch.tensor(dense_matrix)
-    # t = dense_matrix.to(self.device)
-    sum1 = torch.sum((t ** 2) * torch.exp((-t ** 2) / (2 * (Delta ** 2))), dim=0)
-    sum2 = torch.sum(torch.exp((-t ** 2) / (2 * (Delta ** 2))), dim=0)
-    y = 1 / (2 * (Delta ** 2)) * (sum1 / sum2)
-    y = y.cpu().numpy().tolist()
-    return y
+@cuda.jit
+def potential_gpu(y, num, dense_matrix, Delta):
+    idx = cuda.threadIdx.x + cuda.blockDim.x * cuda.blockIdx.x
+    if idx < num:
+        sum1 = 0
+        sum2 = 0
+        for i in range(num):
+            sum1 += (dense_matrix[idx][i] ** 2) * math.exp((-dense_matrix[idx][i] ** 2) / (2 * (Delta ** 2)))
+            sum2 += math.exp((-dense_matrix[idx][i] ** 2) / (2 * (Delta ** 2)))
+        y[idx] = (1 / (2 * (Delta ** 2))) * (sum1 / sum2)
 
 
-# @cuda.jit
-# def gradient_decent_step_one(y, adj_list, num, label):
-#     idx = cuda.threadIdx.x + cuda.blockDim.x * cuda.blockIdx.x
-#     index_flag = idx
-#     if idx < num:
-#         neighbor = adj_list[idx]
-#         low_potential_point = y[idx]
-#         for item in neighbor:
-#             if item == -1:
-#                 break
-#             if low_potential_point > y[int(item)]:
-#                 index_flag = item
-#                 low_potential_point = y[int(item)]
-#     label[idx] = label[int(index_flag)]
+@cuda.jit
+def gradient_decent_step_one(y, adj_list, num, label):
+    idx = cuda.threadIdx.x + cuda.blockDim.x * cuda.blockIdx.x
+    index_flag = idx
+    if idx < num:
+        neighbor = adj_list[idx]
+        low_potential_point = y[idx]
+        for item in neighbor:
+            if item == -1:
+                break
+            if low_potential_point > y[int(item)]:
+                index_flag = item
+                low_potential_point = y[int(item)]
+    label[idx] = label[int(index_flag)]
 
+def find(p, parent):
+    if p != parent[p]:
+        parent[p] = find(parent[p], parent)
+    return parent[p]
 
 def gradient_descent_gpu(delta, g, weight="weight", disconnect_weight=1000):
-
     id = list(nx.to_pandas_adjacency(g))
-    llen = len(nx.nodes(g))
-    label = list(range(llen))
 
-    # threads_per_block = 128
-    # blocks_per_grid = math.ceil(len(nx.nodes(g)) / threads_per_block)
-    # y = np.zeros((len(nx.nodes(g)), 1))
+    label = list(range(len(nx.nodes(g))))
+    threads_per_block = 128
+    blocks_per_grid = math.ceil(len(nx.nodes(g)) / threads_per_block)
+    y = np.zeros((len(nx.nodes(g)), 1))
     dense_matrix = nx.to_numpy_array(g, weight=weight)
-    dense_matrix = dense_matrix[:llen, :llen]
+    dense_matrix = dense_matrix[:len(nx.nodes(g)), :len(nx.nodes(g))]
 
     disconnect_pos = dense_matrix == 0
     # dense_matrix = np.divide(1.0, dense_matrix)
@@ -75,45 +67,28 @@ def gradient_descent_gpu(delta, g, weight="weight", disconnect_weight=1000):
 
     row, col = np.diag_indices_from(dense_matrix)
     dense_matrix[row, col] = 0
-    # potential_gpu[blocks_per_grid, threads_per_block](y, len(label), dense_matrix, delta)
-    y = potential(delta, dense_matrix)
-    # cuda.synchronize()
-    # adj_array = np.ones((len(label), len(label))) * -1
-    # mapping_nodes = nx.nodes(g)
-    # g = nx.convert_node_labels_to_integers(g)
-    # for i in range(len(label)):
-    #     j = 0
-    #     for item in list(g.adj[i]):
-    #         if item == '-1':
-    #             break
-    #         adj_array[i][j] = item
-    #         j += 1
-    # label = np.array(label)
-    # # 转化为向量
-    # y = y.ravel()
-    # gradient_decent_step_one[blocks_per_grid, threads_per_block](y, adj_array, len(label), label)
-    for i, _ in enumerate(label):
-        # try:
-        #     neighbor = g.adj[i].keys()
-        # except KeyError:
-        #     continue
-        neighbor = g.adj[i].keys()
-        index_flag = i
-        low_potential_point = y[i]
-        for item in neighbor:
-            if item >= len(y):
-                continue
-            if low_potential_point > y[item]:
-                index_flag = item
-                low_potential_point = y[item]
-        # low_potential_point = [y[n] for n in neighbor]
-        # low_potential_point.append(y[i])
-        # # low_potential_point = low_potential_point.append(y[i])
-        # index_flag = y.index(min(low_potential_point))
-        label[i] = label[index_flag]
+    potential_gpu[blocks_per_grid, threads_per_block](y, len(label), dense_matrix, delta)
+    cuda.synchronize()
+    adj_array = np.ones((len(label), len(label))) * -1
+    mapping_nodes = nx.nodes(g)
+    g = nx.convert_node_labels_to_integers(g)
+    for i in range(len(label)):
+        j = 0
+        for item in list(g.adj[i]):
+            if item == '-1':
+                break
+            adj_array[i][j] = item
+            j += 1
+    label = np.array(label)
+    # 转化为向量
+    y = y.ravel()
+    # print(y)
+    gradient_decent_step_one[blocks_per_grid, threads_per_block](y, adj_array, len(label), label)
+    start_time = time.perf_counter()
+    
     label_dup = list(set(label))
     k = 0
-
+    """
     for item in label_dup:
         while True:
             if label[item] == item:
@@ -123,11 +98,19 @@ def gradient_descent_gpu(delta, g, weight="weight", disconnect_weight=1000):
                 label_dup[k] = label[item]
                 item = label[item]
         k = k + 1
-
-    # print(len(list(set(label))))
+    """
+    # 新梯度下降
+    for i, elem in enumerate(label):
+            if i != elem: find(i, label)
+    
+    # print(y)
+    # print(label)
+    print(len(label))
+    print("time on GGD: ", time.perf_counter() - start_time)
+    print(len(list(set(label))))
     # print(pd.value_counts(label))
     print("Clusters num: " + str(len(list(set(label)))))
-    return id, label
+    return id, label, mapping_nodes
 
 
 def floyd_dist(g):
@@ -290,13 +273,11 @@ def load_AttributedGraphDataset():
 
 
 def load_CitationFull():
-    # from torch_geometric.datasets import CitationFull
-    import qwe
-    # from torch_geometric.utils.convert import to_networkx
-    dataset = qwe.load_from_npz('cora_ml.npz', 'Cora_ML')
-    g = nx.from_scipy_sparse_matrix(dataset.adj_matrix, parallel_edges=False, create_using=None, edge_attribute='weight')
-    label = dataset.labels
-    # label = data.labels.y.numpy().tolist()
+    from torch_geometric.datasets import CitationFull
+    from torch_geometric.utils.convert import to_networkx
+    dataset = CitationFull(root='./tmp/CitationFull/Cora_ML', name='Cora_ML')
+    g = to_networkx(dataset.data)
+    label = dataset.data.y.numpy().tolist()
     return g, label
 
 
@@ -315,8 +296,8 @@ def evaluation(label_true, label_pred, adj):
     modularity = get_modularity(adj, np.array(label_pred))
     print("Modularity: " + str(modularity))
     if true_cluster_num == pred_cluster_num:
-        acc, nmi, ari, f1, R = clustering.evaluation(label_true, label_pred)
-        print("Acc: %f\tNMI: %f\tARI: %f\tF1: %f\tRecall: %f" % (acc, nmi, ari, f1, R))
+        acc, nmi, ari, f1 = clustering.evaluation(label_true, label_pred)
+        print("Acc: %f\tNMI: %f\tARI: %f\tF1: %f" % (acc, nmi, ari, f1))
     else:
         nmi = normalized_mutual_info_score(label_true, label_pred)
         print("NMI: " + str(nmi))
@@ -348,63 +329,53 @@ def other_algorithms(g, label_true, weight="weight"):
     adj = nx.to_numpy_array(g, weight=weight)
 
     print("\n- Kmeans Method:")
-    time1 = time.perf_counter()
-    kmeans = KMeans(n_clusters=2, random_state=0).fit(adj)
-    print(f'Kmeans algorithm use{time.perf_counter() - time1}')
+    kmeans = KMeans(n_clusters=2, random_state=0, n_init="auto").fit(adj)
     label_pred = kmeans.labels_.tolist()
-    # evaluation(label_true, label_pred, adj)
+    evaluation(label_true, label_pred, adj)
 
     print("\n- Louvain's Method:")
-    time1 = time.perf_counter()
     louvain = Louvain()
-    print(f'Louvain algorithm use{time.perf_counter() - time1}')
     label_pred = louvain.fit_transform(adj)
-    # evaluation(label_true, label_pred, adj)
+    evaluation(label_true, label_pred, adj)
 
-    print("\n- Local and Global Consistency(LPA):")
-    time1 = time.perf_counter()
+    print("\n- Local and Global Consistency(LGC):")
+    from networkx.algorithms import node_classification
+    predicted = node_classification.local_and_global_consistency(g, label_name='club')
     propagation = PropagationClustering()
-    print(f'propagation algorithm use{time.perf_counter() - time1}')
     label_pred = propagation.fit_transform(adj)
-    # evaluation(label_true, label_pred, adj)
+    print(predicted)
+    print(label_pred)
+    evaluation(label_true, predicted, adj)
 
     print("\n- Spectral Clustering:")
-    time1 = time.perf_counter()
-    clustering = SpectralClustering(n_clusters=2,
+    clustering = SpectralClustering(
                                     affinity='precomputed',
                                     assign_labels='kmeans',
                                     random_state=0).fit(adj)
-    print(f'Spectral algorithm use{time.perf_counter() - time1}')
-    # label_pred = clustering.labels_.tolist()
-    # evaluation(label_true, label_pred, adj)
+    label_pred = clustering.labels_.tolist()
+    evaluation(label_true, label_pred, adj)
 
     print("\n- AGNES:")
-    time1 = time.perf_counter()
     clustering = AgglomerativeClustering().fit(adj)
-    print(f'AGNES algorithm use{time.perf_counter() - time1}')
-    # label_pred = clustering.labels_.tolist()
-    # evaluation(label_true, label_pred, adj)
+    label_pred = clustering.labels_.tolist()
+    evaluation(label_true, label_pred, adj)
 
     print("\n- BIRCH:")
-    time1 = time.perf_counter()
     brc = Birch(n_clusters=None).fit(adj)
-    print(f"BIRCH{time.perf_counter() - time1}")
-    # label_pred = brc.labels_.tolist()
-    # evaluation(label_true, label_pred, adj)
+    label_pred = brc.labels_.tolist()
+    evaluation(label_true, label_pred, adj)
 
-    # print("\n- DBSCAN:")
-    # time1 = time.perf_counter()
-    # db = DBSCAN(metric='precomputed', eps=10).fit(adj)
-    # label_pred = db.labels_.tolist()
-    # # print(label_pred)
-    # evaluation(label_true, label_pred, adj)
+    print("\n- DBSCAN:")
+    db = DBSCAN(metric='precomputed', eps=10).fit(adj)
+    label_pred = db.labels_.tolist()
+    # print(label_pred)
+    evaluation(label_true, label_pred, adj)
 
-    # print("\n- OPTICS:")
-    # time1 = time.perf_counter()
-    # optic = OPTICS(metric='precomputed').fit(adj)
-    # label_pred = optic.labels_.tolist()
-    # # print(label_pred)
-    # evaluation(label_true, label_pred, adj)
+    print("\n- OPTICS:")
+    optic = OPTICS(metric='precomputed').fit(adj)
+    label_pred = optic.labels_.tolist()
+    # print(label_pred)
+    evaluation(label_true, label_pred, adj)
 
 
 if __name__ == '__main__':
@@ -415,75 +386,60 @@ if __name__ == '__main__':
     # id, label, mapping_nodes = gradient_descent_gpu(delta=156, g=g, weight=weight)
     # dis = floyd_dist(g)
 
-    # with open("counting_time.txt", "w") as f:
-    #     f.write(f"----cora----\n")
     # g, label_true = load_cora_dataset()
-    # time_now = time.perf_counter()
-    # print("~QC algorithm:\n")
-    # id, label = gradient_descent_gpu(delta=55555, g=g)
-    # print("time is", time.perf_counter() - time_now)
-    # # f.write(f'QC algorithm use{time.perf_counter()-time_now}\n')
-    #
-    # evaluation(label_true, label, nx.to_numpy_array(g))
-    # other_algorithms(g, label_true)
+    # id, label, mapping_nodes = gradient_descent_gpu(delta=1e8, g=g)
+    
 
-    # print(f"----citeseer----\n")
-    g, label_true = load_wiki_dataset()
-    time_now = time.perf_counter()
-    id, label = gradient_descent_gpu(delta=200, g=g)
-    # f.write(f'QC algorithm use{time.perf_counter() - time_now}\n')
-    # print("~QC algorithm:\n")
-    evaluation(label_true, label, nx.to_numpy_array(g))
-    # other_algorithms(g, label_true)
-    #
-    # f.write(f"----karate_club----\n")
-    # g, label_true = load_karate_club()
-    # time_now = time.perf_counter()
-    # id, label = gradient_descent_gpu(delta=1e8, g=g)
-    # f.write(f'QC algorithm use{time.perf_counter() - time_now}\n')
-    # print("~QC algorithm:\n")
-    # evaluation(label_true, label, nx.to_numpy_array(g))
-    # other_algorithms(g, label_true)
-    #
-    # f.write(f"----cora_ml----\n")
-    # g, label_true = load_CitationFull()
-    # time_now = time.perf_counter()
-    # id, label = gradient_descent_gpu(delta=1e8, g=g)
-    # f.write(f'QC algorithm use{time.perf_counter() - time_now}\n')
-    # print("~QC algorithm:\n")
-    # evaluation(label_true, label, nx.to_numpy_array(g))
-    # other_algorithms(g, label_true)
-    #
-    # f.write(f"----coauthor----\n")
-    # g, label_true = load_coauthor()
-    # time_now = time.perf_counter()
-    # id, label = gradient_descent_gpu(delta=1e8, g=g)
-    # f.write(f'QC algorithm use{time.perf_counter() - time_now}\n')
-    # print("~QC algorithm:")
-    # evaluation(label_true, label, nx.to_numpy_array(g))
-    # other_algorithms(g, label_true)
     # g, label_true = load_citeseer_dataset()
     # id, label, mapping_nodes = gradient_descent_gpu(delta=300, g=g)
 
-    # g, label_true = load_pubmed_dataset()
-    # id, label, mapping_nodes = gradient_descent_gpu(delta=1e12, g=g, disconnect_weight=1e5)
+    g, label_true = load_pubmed_dataset()
+    id, label, mapping_nodes = gradient_descent_gpu(delta=1e12, g=g, disconnect_weight=1e5)
 
     # g, label_true = load_wiki_dataset()
     # id, label, mapping_nodes = gradient_descent_gpu(delta=500, g=g)
 
+
     # g, label_true = load_karate_club()
     # id, label, mapping_nodes = gradient_descent_gpu(delta=500, g=g)
+    
 
     # g, label_true = load_coauthor()
     # id, label, mapping_nodes = gradient_descent_gpu(delta=500, g=g)
 
-    # g, label_true = load_AttributedGraphDataset()
-    # id, label, mapping_nodes = gradient_descent_gpu(delta=100, g=g)
+
+    # g, label_true = load_CitationFull()
+    # id, label, mapping_nodes = gradient_descent_gpu(delta=200, g=g)
+    # nx.write_gml(g, "coraml.gml")
+
+    # dis = floyd_dist(g)
+    """
+    test example
+    a = [[0, 1, 0, 0, 1, 0],
+         [1, 0, 0, 0, 1, 1],
+         [0, 0, 0, 1, 0, 1],
+         [0, 0, 1, 0, 0, 1],
+         [1, 1, 0, 0, 0, 0],
+         [0, 1, 1, 1, 0, 0]]
+    g = nx.from_numpy_array(np.array(a))
+    dis = floyd_dist(g)
+    """
+
+    # dis = np.load("dis_mat.npy")
+    # dis = np.load("cora_dis.npy")
+    # dis[dis == np.inf] = 1000
+    # dis = normalize(dis, norm="l2")
+    # dis = normalize_adj(dis, symmetry=False)
 
     # label_fill(label)
-    # output_id_and_label(id, label)
+    # output_id_and_label(id, label_true, output_path='cora_nodes.csv')
     # result_visualization(label, mapping_nodes)
 
     # np.fill_diagonal(dis, 0)
     # silhouette_score = metrics.silhouette_score(dis, label, metric="precomputed")
     # print(silhouette_score)
+
+    # print("~QC algorithm:")
+    # evaluation(label_true, label, nx.to_numpy_array(g))
+
+    # other_algorithms(g, label_true)
